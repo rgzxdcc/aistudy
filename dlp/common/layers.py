b843f307d56568547ea7a730b93bab28882387e3
dcc <rgzxdcc@sina.com>
2026-08-14 18:31:39 +0800

    再探word2vec

--- dlp/common/layers.py
+++ dlp/common/layers.py
@@ -125,3 +125,21 @@ class SigmoidWithLoss:
 
         dx = (self.y - self.t) * dout / batch_size
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backwrad(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        return None
