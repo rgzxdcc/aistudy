21306255403e977e9e2192095008128ab6d5d351
xuwen326 <861770969@qq.com>
2026-09-04 16:06:51 +0800

    完成Docker环境安装，镜像制作，验证容器运行后正常提供LLM-CHAT服务

--- llm_app/01_api_basics/app.py
+++ llm_app/01_api_basics/app.py
@@ -6,8 +6,8 @@ from fastapi import FastAPI
 import openai
 from pydantic import BaseModel
 
-
-client_local = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1")
+# 将ollama的网址改为配置读取，解决在Docker中访问错误问题
+client_local = OpenAI(api_key="ollama", base_url=os.getenv("OLLAMA_BASE_URL", "http://localhost:11434/v1"))
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url=os.getenv("DEEPSEEK_BASE_URL"))
@@ -50,3 +50,33 @@ async def chat_stream(req: ChatRequest):
                 yield delta
 
     return StreamingResponse(stream(), media_type="text/event-stream")
+
+# Docker重新烧制命令
+# Docker build -t llm-chat .
+
+# Docker停止旧容器
+# Docker ps
+# Docker stop <容器ID>
+
+# Docker启动新容器（跑云端模型）
+# docker run --rm --env-file /Users/xuwen/Documents/dcc_study/aistudy/.env -p 8000:8000 llm-chat
+
+# Docker跑本地模型运行命令
+# docker run --rm --env-file /Users/xuwen/Documents/dcc_study/aistudy/.env \
+#   -e OLLAMA_BASE_URL=http://host.docker.internal:11434/v1 \
+#   -p 8000:8000 llm-chat
+
+# 终端连接验证： chat
+# curl -X POST http://127.0.0.1:8000/chat \
+#   -H "Content-Type: application/json" \
+#   -d '{"messages":[{"role":"user","content":"用一句话解释什么是Docker"}]}'
+
+# 终端连接验证： chat_stream
+# curl -N -X POST http://127.0.0.1:8000/chat_stream \
+#   -H "Content-Type: application/json" \
+#   -d '{"messages":[{"role":"user","content":"从1数到100"}]}'
+
+# 终端连接验证： chat_local
+# curl -X POST http://127.0.0.1:8000/chat/local \
+#   -H "Content-Type: application/json" \
+#   -d '{"messages":[{"role":"user","content":"用一句话解释什么是Docker"}]}'
